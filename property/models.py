a1f0749663d4d64985d7bdf1dab6de0c098c654a
killla <killa.granet@gmail.com>
2020-03-28 08:54:35 +0500

    Add claims

--- property/models.py
+++ property/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Flat(models.Model):
@@ -26,3 +27,8 @@ class Flat(models.Model):
 
     def __str__(self):
         return f"{self.town}, {self.address} ({self.price}р.)"
+
+class Claim(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Кто жаловался')
+    flat = models.ForeignKey(Flat, on_delete=models.CASCADE, verbose_name='Квартира, на которую пожаловались')
+    text = models.CharField(max_length=20, verbose_name='Текст жалобы')
